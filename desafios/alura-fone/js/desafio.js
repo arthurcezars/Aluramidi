fc56c546a9c742470458328270e37c7f68ae17d7
arthurcezars <arthurcezars@gmail.com>
2022-03-13 14:44:42 -0300

    Alterando o comportamento do uso do teclado com o Aluramid e a função 'tocaSom' para funcionar somente com o elemento do tipo audio.

--- desafios/alura-fone/js/desafio.js
+++ desafios/alura-fone/js/desafio.js
@@ -7,5 +7,13 @@ const listaDeTeclas = document.querySelectorAll('.teclado > input[type=button]')
 console.log(listaDeTeclas);
 
 listaDeTeclas.forEach( (tecla) => {
-    tecla.onclick = function(){ digita(tecla) };
+    tecla.onclick = function(){ digita(tecla); }
+
+    tecla.onkeydown = function(evento) { 
+        if (evento.code == 'Enter' || evento.code == 'Space') {
+            tecla.classList.add('ativa'); 
+        }
+    }
+
+    tecla.onkeyup = function() { tecla.classList.remove('ativa'); }
 })
--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 function tocaSom(tecla) {
     let idAudio = `#som_${tecla}`;
 
-    document.querySelector(idAudio).play();
+    elemento = document.querySelector(idAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas.forEach( (tecla) => {
-    tecla.onclick = function() { tocaSom(tecla.classList[1]) };
+    tecla.onclick = function() { tocaSom(tecla.classList[1]); }
+
+    tecla.onkeydown = function(evento) { 
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa'); 
+        }
+    }
+
+    tecla.onkeyup = function() { tecla.classList.remove('ativa'); }
 })
